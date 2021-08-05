ec1914dd99e4c6fcbb34df7c36a09c974decfdb1
Peter Abah <peterabah.ob@gmail.com>
2021-08-05 21:54:03 +0100

    add associations between users and freind_requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :sent_friend_requests, class_name: 'FriendRequest', 
+                                  foreign_key: :sender_id, dependent: :destroy
+  has_many :recieved_friend_requests, class_name: 'FriendRequest', 
+                                      foreign_key: :receiver_id,
+                                      dependent: :destroy
 end
