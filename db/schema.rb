da752b9e51553ba88b47352b3969f03ac92ef3f1
Peter Abah <peterabah.ob@gmail.com>
2021-08-05 21:42:01 +0100

    create FriendRequest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_200540) do
+ActiveRecord::Schema.define(version: 2021_08_05_203557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "sender_id_id"
+    t.bigint "receiver_id_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["receiver_id_id"], name: "index_friend_requests_on_receiver_id_id"
+    t.index ["sender_id_id"], name: "index_friend_requests_on_sender_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_08_05_200540) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friend_requests", "users", column: "receiver_id_id"
+  add_foreign_key "friend_requests", "users", column: "sender_id_id"
 end
