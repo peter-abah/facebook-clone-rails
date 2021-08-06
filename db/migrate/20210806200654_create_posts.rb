04e28eb20261e88836370fc7f5a67e070cacb74f
Peter Abah <peterabah.ob@gmail.com>
2021-08-06 21:09:18 +0100

    create Post model

---
+++ db/migrate/20210806200654_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.text :body
+      t.references :user, index: true, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
