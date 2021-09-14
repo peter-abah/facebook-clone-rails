8a4170ceb6089b16a5f474b148a3298f4e6c5b3c
Peter Abah <peterabah.ob@gmail.com>
2021-09-14 16:24:07 +0100

    create search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,10 @@ class SearchController < ApplicationController
   def search
     query = params[:query]
 
-    render :search unless query && !query.empty?
+    render :search && return if query.nil? || query.empty?
 
-    @posts = Post.where('body LIKE %?%', query)
-    @users = User.where('name LIKE %?%', query)
+    query = query.downcase
+    @posts = Post.where('LOWER(body) LIKE ?', "%#{query}%")
+    @users = User.where('LOWER(name) LIKE ?', "%#{query}%")
   end
 end
