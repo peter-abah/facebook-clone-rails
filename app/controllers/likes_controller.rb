1d8b54f8ff72703f814600a5cc79c2c019f5e9d1
Peter Abah <peterabah.ob@gmail.com>
2021-10-21 09:00:32 +0100

    fix like bug that redirects to home page when a user likes a post on a user profile page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = @post.likes.create!(like_params)
 
-    redirect_to posts_path
+    redirect_back fallback_location: posts_path
   end
 
   def destroy
     @like = Like.find(params[:id])
     @like.destroy
-    redirect_to posts_url
+    redirect_back fallback_location: posts_path
   end
 
   def like_params
